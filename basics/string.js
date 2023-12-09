06c655c61d60cb7d224d97fb9223b4b268b34707
aman pandey <amanpandey39563@gmail.com>
2023-12-09 14:18:51 +0530

    added numbers and maths

--- basics/string.js
+++ basics/string.js
@@ -1,35 +1,43 @@
-const name = "hitesh"
-const repoCount = 50
+const myName = "aman";
+const repo = 50;
 
-// console.log(name + repoCount + " Value");
+// console.log(name + repo + " value"); old method or technique
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+console.log(`Hello my name is ${myName} and my repo count is ${repo}`);
+// this is modern way called as string interpolation 
 
-const gameName = new String('hitesh-hc-com')
+const gameName = new String("Aman-dev-hc") // another method of declaring string
 
-// console.log(gameName[0]);
-// console.log(gameName.__proto__);
-
-
-// console.log(gameName.length);
-// console.log(gameName.toUpperCase());
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
+console.log(gameName.indexOf('d'));
 
-const newString = gameName.substring(0, 4)
+const newString = gameName.substring(0,4);
 console.log(newString);
-
-const anotherString = gameName.slice(-8, 4)
+const anotherString = gameName.slice(-6,4);
 console.log(anotherString);
 
-const newStringOne = "   hitesh    "
+const newStringOne = "   aman   " // for avoiding spaces
 console.log(newStringOne);
 console.log(newStringOne.trim());
 
-const url = "https://hitesh.com/hitesh%20choudhary"
+const url = "https://aman.com/aman%20pandey"
+console.log(url.replace('%20','-')); // for replacing unnecessary stuff from url
+console.log(url.includes('aman')); // for checking elements of url
 
-console.log(url.replace('%20', '-'))
+console.log(gameName.split('-')); // to convert string into array based on (-) or anything 
 
-console.log(url.includes('sundar'))
 
-console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
+
+
+

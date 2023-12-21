bf96817be194b6827b66ec42050cb35e5338a778
aman pandey <amanpandey39563@gmail.com>
2023-12-21 12:05:34 +0530

    object destructuring

--- basicspart2/object2.js
+++ basicspart2/object2.js
@@ -1,5 +1,5 @@
  //const tinderUser = new Object() // singleton object
-const tinderUser = {} // another way to declare object but it is non-singleton object
+/*const tinderUser = {} // another way to declare object but it is non-singleton object
 
 tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
@@ -56,4 +56,20 @@ console.log(Object.values(tinderUser)); // gives o/p of all values
 
 console.log(Object.entries(tinderUser)); // gives o/p of combine or both key-value pair
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn')); // sometimes you ask the value which is not present in database which cause crash hence to avoid this condition simply we can ask to database using his method
+console.log(tinderUser.hasOwnProperty('isLoggedIn')); // sometimes you ask the value which is not present in database which cause crash hence to avoid this condition simply we can ask to database using his method*/
+
+
+// ****************** Object Destructuring ************************************************
+
+const course = {
+    course: "js in hindi",
+    Price: "999",
+    courseInstructor: "aman"
+
+}
+console.log(course.courseInstructor); // ye toh common method hai hi but its lengthy to write course.courseInstructor always hence we use object destructuring method so see below
+
+const {courseInstructor: instructor} = course // we can also change name courseInstructor to instructor like this
+
+// console.log(courseInstructor);
+console.log(instructor);

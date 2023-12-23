aeb86eec442ea77eeb51648154694bb12b862cd5
aman pandey <amanpandey39563@gmail.com>
2023-12-23 11:46:57 +0530

    scopes in detail

--- basicspart3/scopes.js
+++ basicspart3/scopes.js
@@ -13,3 +13,47 @@ if (true) {
 // console.log(b);
 // console.log(c);
 
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+function one(){
+    const username = "Aman"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "Aman"
+    if (username === "Aman") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}

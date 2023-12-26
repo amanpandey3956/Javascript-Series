a13b7de03e93a2c6248ff5003f59a1b796d32628
aman pandey <amanpandey39563@gmail.com>
2023-12-26 21:02:23 +0530

    for In loop updated

--- iterations/four.js
+++ iterations/four.js
@@ -1,3 +1,7 @@
+// as we know object is not capable of iteration in for of loop but it is possible with for in loop
+
+// for In loop in object
+
 const myObject = {
     js: 'javascript',
     cpp: 'C++',
@@ -6,21 +10,33 @@ const myObject = {
 }
 
 for (const key in myObject) {
-    //console.log(`${key} shortcut is for ${myObject[key]}`);
+    console.log(`${key} shortcut is for ${myObject[key]}`);
 }
+
+// ****************************************************************************************
+
+// for In loop in array
 
 const programming = ["js", "rb", "py", "java", "cpp"]
 
 for (const key in programming) {
-    //console.log(programming[key]);
+    console.log(programming[key]);
 }
 
-// const map = new Map()
-// map.set('IN', "India")
-// map.set('USA', "United States of America")
-// map.set('Fr', "France")
-// map.set('IN', "India")
+// ****************************************************************************************
 
-// for (const key in map) {
-//     console.log(key);
-// }
+// for In loop in Map
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+for (const key in map) {
+     console.log(key);
+}
+
+// o/p - null or nothing bcoz Map is not iterable 
+
+// ****************************************************************************************

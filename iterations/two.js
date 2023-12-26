dfde18f4c796989ee89255894c17b869eebf9635
aman pandey <amanpandey39563@gmail.com>
2023-12-26 13:19:21 +0530

    while loop added

---
+++ iterations/two.js
@@ -0,0 +1,28 @@
+// while loop 
+
+let index = 0
+while (index <= 10) {
+     console.log(`Value of index is ${index}`);
+     index = index + 2
+ } // o/p - 0,2,4,8,10
+//*******************************************************************
+
+let myArray = ['flash', "batman", "superman"]
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+//********************************************************************
+
+let score = 11
+
+// do while loop - in this loop first do part will run then while condition is checked
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+// o/p - 11
+
+// **********************************************************************
